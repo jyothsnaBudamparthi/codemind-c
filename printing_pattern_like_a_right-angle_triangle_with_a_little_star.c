f9f6abf39a9a9e709ea00ce031e1e496e8ee6d5b
jyothsnaBudamparthi <147488681+jyothsnaBudamparthi@users.noreply.github.com>
2023-12-05 22:19:04 +0530

    #include<stdio.h> int main() {     int j,i,r=0;     scanf("%d",&r);     for(i=1;i<=r;i++)     {         for(j=1;j<=i;j++)         {         printf("*");         }     printf(" ");     }     }

---
+++ printing_pattern_like_a_right-angle_triangle_with_a_little_star.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int j,i,r=0;
+    scanf("%d",&r);
+    for(i=1;i<=r;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+        printf("*");
+        }
+    printf("
+");
+    }
+    }
