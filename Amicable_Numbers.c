f5046d5da108b76074cc21f2887201db28a16fc9
jyothsnaBudamparthi <147488681+jyothsnaBudamparthi@users.noreply.github.com>
2024-01-01 19:40:27 +0530

    #include<stdio.h> int main() {     int a,b,i,x=0,y=0;     scanf("%d%d",&a,&b);     for(int i=1;i<a;i++)     {         if(a % i ==0)         {             x=x+i;         }     }     for(int i=1;i<b;i++)     {     if(b % i==0)     {         y=y+i;     } } if((a==y)&& (b==x)) {     printf("Amicable"); }else {     printf("Not Amicable"); } }

---
+++ Amicable_Numbers.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,x=0,y=0;
+    scanf("%d%d",&a,&b);
+    for(int i=1;i<a;i++)
+    {
+        if(a % i ==0)
+        {
+            x=x+i;
+        }
+    }
+    for(int i=1;i<b;i++)
+    {
+    if(b % i==0)
+    {
+        y=y+i;
+    }
+}
+if((a==y)&& (b==x))
+{
+    printf("Amicable");
+}else
+{
+    printf("Not Amicable");
+}
+}
